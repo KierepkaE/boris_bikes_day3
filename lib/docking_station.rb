c106e99ae66dadae90b64f92209068d8c47e341d
kierepkae <kierepkaedyta@gmail.com>
2019-07-25 14:44:06 +0100

    updated docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,12 @@ class DockingStation
     attr_reader :bike
 
     def release_bike
-<<<<<<< HEAD
+
       fail "There are no bikes" unless @bike
       @bike = Bike.new
-=======
+
           Bike.new
->>>>>>> 2a971714e72be1d34afade4fce61d8d57226db6b
+
     end
 
     def dock(bike)

6f1faa73fb3e2e59a5ea9ef1be81dc8d9dabd145
kierepkae <kierepkaedyta@gmail.com>
2019-07-25 14:11:07 +0100

    initial commit

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,29 @@
+require 'docking_station'
+
+describe DockingStation do
+
+  it " responds to release_bike" do
+    expect(subject).to respond_to :release_bike
+  end
+
+  it " This gets a bike " do
+     bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+end
